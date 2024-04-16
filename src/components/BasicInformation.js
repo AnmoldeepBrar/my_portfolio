f8284bd94aef565d153575cd50f14bc5d53f1e33
AnmoldeepBrar <Anmoldeepkaur2910@gmail.com>
2024-04-16 10:37:38 -0500

    first commit

---
+++ src/components/BasicInformation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const BasicInformation = () => {
+  return (
+    <div className="basic-info-container">
+      <h1>Full Stack Web developers</h1>
+      <h2>Web programmer</h2>
+      <div className="info-box">
+        <p>Welcome to My Portfolio</p>
+        <p>
+          Hey there! I'm Anmoldeep, a dedicated and innovative software developer deeply immersed in the world of web development. My journey revolves around transforming concepts into practical and refined software solutions. With a meticulous approach and a genuine passion for coding, I'm committed to crafting intuitive and effective applications that deliver impactful user experiences.
+        </p>
+        <p>
+          My expertise lies in full-stack web development, where I thrive in bringing visions to fruition, right from inception to deployment. Whether it's front-end design or back-end functionality, I ensure seamless integration and optimal performance throughout. I'm proficient with a variety of technologies, including React, Vue.js, Node.js, and Express, constantly expanding my skill set to stay ahead in this ever-evolving industry.
+        </p>
+        <p>
+          Contact Me: Feel free to reach out to me at <a href="mailto:anmoldeepbrar700@gmail.com">anmoldeepbrar700@gmail.com</a>, or <a href="tel:2045583366">204-558-3366</a> to discuss potential collaborations or simply chat about all things tech!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BasicInformation;
---
+++ src/components/Work.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import projectImage from '../images/p2.jpg';
+import projectImages from '../images/p1.jpg'; 
+
+const Work = () => {
+  return (
+    <div>
+      <h2>Work/Projects</h2>
+      <ul>
+        <li>
+          <h3>Brar Book Store</h3>
+          <p>
+            Developed a dynamic bookstore website utilizing HTML, CSS, and JavaScript.<br />
+            Implemented interactive features including search functionality, category filters, and responsive design to enhance user experience.<br />
+            Utilized modern web development techniques to create an intuitive and visually appealing interface, resulting in improved accessibility and user engagement.
+          </p>
+          <img src={projectImages} alt="Project 2" />
+          <a href="https://www.brars.com/">Project Link</a>
+        </li>
+        <li>
+          <h3>Online Library System</h3>
+          <p>
+            Designed and implemented a robust online library system using PHP.<br />
+            Integrated Database Management System for efficient data management and retrieval, ensuring seamless operation of the library platform.<br />
+            Implemented user authentication and authorization functionalities to ensure secure access to library resources, enhancing data privacy and system integrity.
+          </p>
+          <img src={projectImage} alt="Project 2" />
+          <a href="https://www.brars.com/">Project Link</a>
+        </li>
+        {/* Add more projects here */}
+      </ul>
+    </div>
+  );
+};
+
+export default Work;
